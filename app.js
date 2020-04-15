5331a46626363168db01b64e2bb511d9b8423210
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-04-15 16:24:10 -0300

    Erros preview

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const routerUsers = require("./src/routes/users");
 const routerExercises = require("./src/routes/exercises");
 
+app.use(morgan("dev"));
+
 app.use("/users", routerUsers);
 app.use("/exercises", routerExercises);
+
+//Erro - Router Undefined
+app.use((req, res, next) => {
+  const err = new Error("Não encontrado");
+  err.status(404);
+  next(err);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    error: {
+      mensage: error.message,
+    },
+  });
+});
 
 module.exports = app;
